3b85517ff20665f59d0eb21d194aeecc5548d2a6
dennis <tmacdennisdddd@gmail.com>
2026-04-23 13:40:07 +0800

    fix(ui): wire new chat shortcut (#345)
    Related to #181

--- src/hooks/useModelSelection.ts
+++ src/hooks/useModelSelection.ts
@@ -3,7 +3,6 @@
  */
 import { useState, useEffect, useCallback } from 'react';
 import { GATEWAY_ENDPOINTS } from '../config/gatewayConfig';
-import { gatewayFetch } from '../config/gatewayConfig';
 
 export interface AvailableModel {
   id: string;
